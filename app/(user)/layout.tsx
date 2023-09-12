07a332367aa612fceccaf54ee6a739d944b6063c
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-09-12 21:24:04 +0700

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navigation from './Components/Navigation'
 import Footter from './Components/Footer'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
