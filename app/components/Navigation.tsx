2872cf4bdf5706a74ba1f8eebd9cf7b8f992f88e
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2023-09-12 21:45:17 +0700

    add react-icon

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+"use client";
+
+import React, {useState} from 'react'
 import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navigation = () => {
+    const [menuOpen, setmenuOpen] = useState(false)
+
+    const  handleMenu = () => {
+        setmenuOpen(!menuOpen)
+    }
+
+
+    const handleClose = () => {
+        setmenuOpen(menuOpen)
+    }
+
   return (
     <div className='border-b'> 
         <nav className="bg-white border-gray-200 px-5">
@@ -9,15 +23,20 @@ const Navigation = () => {
                 <div className="flex flex-row pl-10">
                     <Link href="/" className="flex flex-row justify-around">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap">JTools Logo</span>
-                    </Link>                  
+                    </Link>
+                    <div className="mr-6"><AiOutlineMenu onClick={handleMenu} className="sm:hidden text-2xl cursor-pointer justify-end ml-"/></div>                  
                 </div>
-                <div className=" w-full justify-end" id="navbar-dropdown">
-                    <ul className="flex flex-col md:flex-row sm:justify-end font-medium p-4 lg:text-lg md:text-md sm:text-sm">
-                        <li><Link href="/home" className="py-2 pl-3 pr-4 text-grey rounded hover:bg-gray-100 hover:text-blue-400">หน้าแรก</Link></li>
-                        <li><Link href="/about-us" className=" py-2 pl-3 pr-4 text-grey rounded  hover:bg-gray-100 hover:text-blue-400" >เกี่ยวกับเรา</Link></li>
-                        <li><Link href="/news" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400">ข่าวสารและกิจกรรม</Link></li>
-                        <li><Link href="/customer-service" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400">สินค้าและบริการ</Link></li>
-                        <li><Link href="/contact-us" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400">ติดต่อเรา</Link></li>
+                
+                <div className={menuOpen ? 'left-0 top-0 w-[65%] h-screen relative bg-[#ecf0f3] ease-in duration-500 flex flex-col justify-start z-50': 'hidden'}>
+                    <div className="flex absolute top-6 right-6 w-full justify-end items-center">
+                        <AiOutlineClose onClick={handleMenu} className="text-2xl text-black mb-4" />
+                    </div>
+                    <ul className="flex flex-col justify-center items-center mt-20 font-medium p-4 lg:text-lg md:text-md sm:text-sm">
+                        <li><Link href="/home" className="py-2 pl-3 pr-4 text-grey rounded hover:bg-gray-100 hover:text-blue-400 mb-4">หน้าแรก</Link></li>
+                        <li><Link href="/about-us" className=" py-2 pl-3 pr-4 text-grey rounded  hover:bg-gray-100 hover:text-blue-400 mb-4" >เกี่ยวกับเรา</Link></li>
+                        <li><Link href="/news" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400 mb-4">ข่าวสารและกิจกรรม</Link></li>
+                        <li><Link href="/customer-service" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400 mb-4">สินค้าและบริการ</Link></li>
+                        <li><Link href="/contact-us" className=" py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-400 mb-4">ติดต่อเรา</Link></li>
                     </ul>
                 </div>
             </div>
