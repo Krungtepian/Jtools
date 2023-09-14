fa0903271de2cc08367473fc083db2107139599f
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-09-14 09:43:13 +0700

    Update page.tsx

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -7,11 +7,11 @@ export default function Login() {
   const [user, setUser] = useState("")
   const [password, setPassword] = useState("")
 
-  return <div>
-    Test
-    <form method="POST">
-      <input type="text" autoComplete="false"/>
-      <input type="password" autoComplete="false"/>
-    </form>
-  </div>
+  return <div className="flex justify-center w-full h-full p-10 bg-slate-600">
+      <form method="POST" className="shadow-2xl">
+        <p className="text-zinc-400">Form test </p>
+        <input type="text" autoComplete="false"/>
+        <input type="password" autoComplete="false"/>
+      </form>
+    </div>
 }
