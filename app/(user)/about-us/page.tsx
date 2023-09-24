cb075fb340f9cdc060d5d6e4be3d6af736929876
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2023-09-24 22:52:34 +0700

    fix some CHAI mistake

--- app/(user)/about-us/page.tsx
+++ app/(user)/about-us/page.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 type Props = {}
 
 export default function AboutUs({}: Props) {
-  return (
-    <div>
-      <h1 className='px64 py-52 text-center font-semibold text-2xl'>About us</h1>
-        
-    </div>
-  )
+return (
+  <div>
+    <h1 className='py-52 text-center font-semibold text-2xl'>About us</h1>
+  </div>
+)
 }
 
 // fix this 
