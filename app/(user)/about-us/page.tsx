299f1dd505b210550ad7626a9da6d86bc39a67b6
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-11-07 04:12:22 +0700

    Update page.tsx

--- app/(user)/about-us/page.tsx
+++ app/(user)/about-us/page.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 export default function AboutUs({}: Props) {
   return (
-    <div>
+    <div className='mt-[10vh]'>
       <h1 className='px64 py-52 text-center font-semibold text-2xl'>About us</h1>
-        
+      
     </div>
   )
 }
