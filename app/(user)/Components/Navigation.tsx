1c5e02012b1a61f01722d365d8fbcde903af9ba8
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-11-07 00:25:44 +0700

    Updated Navigation

--- app/(user)/Components/Navigation.tsx
+++ app/(user)/Components/Navigation.tsx
@@ -7,13 +7,11 @@ import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 const Navigation = () => {
     const [navbar, setNavbar] = useState(false);
   return (
-    <div>
-      <nav className="w-full bg-white fixed top-0 left-0 right-0 z-10">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-          <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <Link href="/">
-                <h2 className="text-2xl text-black font-bold ">Jtool logo</h2>
+      <nav className="w-full bg-white top-0 left-0 right-0 z-10 ">
+        <div className="mx-auto md:items-center md:flex lg:px-4 md:px-2 sm:px-2 w-full justify-between lg:text-base ">
+          <div className="flex flex-row items-center justify-between py-3 md:py-5 md:block">
+              <Link href="/" className='max-w-fit'>
+                <h2 className="text-2xl font-semibold w-fit">Jtool logo</h2>
               </Link>
               <div className="md:hidden">
                 <button
@@ -27,46 +25,39 @@ const Navigation = () => {
                   )}
                 </button>
               </div>
-            </div>
           </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'p-12 md:p-0 block' : 'hidden'
-              }`}
-            >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-6 text-xl text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-gray-100 hover:text-blue-400">
-                  <Link href="/home" onClick={() => setNavbar(!navbar)}>
-                    หน้าเเรก
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100">
-                  <Link href="/about-us" onClick={() => setNavbar(!navbar)}>
-                    เกี่ยวกับเรา
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100">
-                  <Link href="/news" onClick={() => setNavbar(!navbar)}>
-                    ข่าวสารและกิจกรรม
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100">
-                  <Link href="/customer-service" onClick={() => setNavbar(!navbar)}>
-                    สินค้าและบริการ
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100">
-                  <Link href="/contact-us" onClick={() => setNavbar(!navbar)}>
-                    ติดต่อพวกเรา
-                  </Link>
-                </li>
-              </ul>
-            </div>
+          <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
+                <ul className=" md:h-auto items-center flex flex-row w-full justify-end">
+                    <li className=" lg:text-xl md:text-sm sm:text-xs text-black py-1 md:px-5 text-center border-b-2 md:border-b-0  hover:bg-gray-100 hover:text-blue-400 items-center rounded-lg active:scale-105">
+                      <Link href="/home" onClick={() => setNavbar(!navbar)}>
+                        หน้าเเรก
+                      </Link>
+                    </li>
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-lg active:scale-105">
+                      <Link href="/about-us" onClick={() => setNavbar(!navbar)}>
+                        เกี่ยวกับเรา
+                      </Link>
+                    </li>
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-lg active:scale-105">
+                      <Link href="/news" onClick={() => setNavbar(!navbar)}>
+                        ข่าวสารและกิจกรรม
+                      </Link>
+                    </li>
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100 rounded-lg active:scale-105">
+                      <Link href="/service" onClick={() => setNavbar(!navbar)}>
+                        สินค้าและบริการ
+                      </Link>
+                    </li>
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100 rounded-lg active:scale-105">
+                      <Link href="/contact-us" onClick={() => setNavbar(!navbar)}>
+                        ติดต่อพวกเรา
+                      </Link>
+                    </li>
+                </ul>
           </div>
         </div>
       </nav>
-    </div>
+
   );
             }
   export default Navigation ; 
