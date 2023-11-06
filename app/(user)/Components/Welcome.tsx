6a82ddc1218d91a82e2a5143694dd1418e143750
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-11-07 00:25:51 +0700

    Update Welcome.tsx

--- app/(user)/Components/Welcome.tsx
+++ app/(user)/Components/Welcome.tsx
@@ -4,17 +4,17 @@ type Props = {}
 
 export default function Welcome({}: Props) {
   return (
-    <div className=' flex justify-center border flex-col shadow-md px-64 py-52 mb-5'>
-        <div className='flex flex-col text-center justify-center'>
-            <div className="grid gap-1 lg:font-lg md:text-sm sm:text-xs justify-center text-center">
-              <div className='lg:font-5xl md:text-3xl sm:text-xl  mb-5'>Welcome to JTools Company</div>
-            <span className=' '>บริษัท เจ ทูลส์ เทคโนโลยี จำกัด (JTools) เป็นผู้นำด้านวิศวกรรม ให้บริการออกแบบและผลิตเครื่องจักร ชิ้นส่วนแม่นยำ อุปกรณ์ Jig & Fixture และเครื่องมือพิเศษ </span>
-            <span className=''>รวมถึงระบบอัตโนมัติ ท่ามกลางประสบการณ์แบ่งปันในอุตสาหกรรมหลากหลาย </span>
-              <span className=' '>เช่น การผลิตเหล็ก กระดาษ ปูนซีเมนต์ พลังงาน และปิโตรเคมี สู่ภาครัฐและรัฐวิสหากิจต่างๆ </span>
-              <span className=''>ที่ JTools เราพร้อมสร้างความเยี่ยมยอดด้วยวิศวกรรมและนวัตกรรมในทุกโครงการ.</span>
-              </div>
-        </div>
-</div>
+    <div className='flex justify-center shadow-md w-full'>
+      <div className='flex flex-col text-center justify-center'>
+          <div className="grid gap-1 lg:text-lg md:text-sm sm:text-xs justify-center text-center">
+            <div className='lg:font-5xl md:text-3xl sm:text-xl  mb-5'>Welcome to JTools Company</div>
+                      บริษัท เจ ทูลส์ เทคโนโลยี จำกัด
+          เป็นผู้นำเรื่อง การออกแบบงานออโตเมชั่น Automation เครื่องจักรที่ลดเวลาการผลิต และเพิ่มปริมาณงาน และรักษาคุณงานให้ได้ตามมาตราฐาน โดยการใช้เทคโนโลยี ที่เหมาสมและควบคุมได้ง่าย
+          เราเชี่ยวชาญในการผลิตและออกแบบเครื่องจักรอัตโนมัติ เครื่องมือพิเศษในขบวนการผลิตของลูกค้า
+          ด้วยการใช้ ซอพแวร์ชั้นนำที่น่าเชื่อถือ ถูกต้อง มีลิขสิทธิ,ให้บริการผลิตภัณฑ์เครื่องจักรตามความต้องการของลูกค้า,รับผลิตงานตามแบบงานและรับผลิตที่มีความละเอียดสูง Precision
+            </div>
+          </div>
+    </div>
 
   )
 }
