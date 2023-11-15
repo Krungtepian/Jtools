de9989c9279662fb0dbc735033ea67147efb0070
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2023-11-15 17:08:46 +0700

    Update About us

---
+++ app/(user)/about-us/Components/Aboutus.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <section className={`flex flex-col bg-white py-5 text-2xl md:text-3xl`}>
+          <div className="container mx-auto px-11 text-center">
+            <h1 className="leading-tight max-w-5xl mx-auto text-4xl tracking-tight">
+                About us
+            </h1>
+            <h2 className="text-2xl mt-4">
+                Business detail
+                </h2>
+<strong className="text-xl leading-tight tracking-tight">สโลเเกน</strong>
+<p className="text-xl leading-tight tracking-tight">สร้างสรรค์ผลิตภัณฑ์ที่ดีมีคุณค่าให้ลูกค้าอย่างยั่งยืน</p>
+<strong className="text-xl leading-tight tracking-tight">วิสัยทัศน์</strong>
+<p className="text-xl leading-tight tracking-tight">เพิ่มมูลค่าผลิตภัณฑ์โดยการทำ Automation ให้เป็นที่ยอมรับ ในด้านคุณภาพ บริการ ราคา
+และขยายการขายไปยังกลุ่มลูกค้าใหม่</p>
+<strong className="text-xl leading-tight tracking-tight">พันธกิจ</strong>
+<p className="text-xl leading-tight tracking-tight">นำเทคโนโลยีใหม่ๆมาใช้ตั้งแต่ขบวนการออกแบบ การผลิต และการประกันคุณภาพสินค้าตั้งแต่วัตถุดิบจนเป็นผลิตภัณฑ์</p>
+<p className="text-xl leading-tight tracking-tight">ขยายการไปยังกลุ่มลูกค้าใหม่ๆ อย่างต่อเนื่อง</p>
+<p className="text-xl leading-tight tracking-tight">เสริมสร้างและพัฒนาบุคลากรให้สามารถรับงาน Automation ให้มากที่สุด</p>
+          </div>
+          <div className="container mx-auto px-11 text-center mt-10">
+<h3 className="text-xl leading-tight tracking-tight font-bold ">หัวข้อสำคัญในการดำเนินงาน</h3>
+<div className="text-xl leading-tight tracking-tight">
+<p><strong>การตลาด</strong>-ขยายฐานการขายอย่างไม่หยุดยั้ง</p>
+<p><strong>คุณภาพ</strong>-สร้างระบบและจัดการคุณภาพของสินค้าผลิต</p>
+<p><strong>เทคโนโลยี</strong>-ใช้เทคโนโลยีที่เหมาะสมและควบคุมได้</p>
+<p><strong>การผลิต</strong>-ยกระดับประสิทธิภาพการผลิต</p>
+<p><strong>ต้นทุน</strong>-ขยายงานการผลิตให้เต็มกำลังความสามารถของคนและเครื่องจักร</p>
+<p><strong>บุคลากร</strong>-สร้างบุคลากรและองค์กรที่แข็งแกร่ง</p>
+<p><strong>อื่นๆ</strong>-ควบคุมความเสี่ยง,รักษา 5ส,รักษา Compliance, Percision</p>               
+</div>
+          </div>
+          <div className='container mx-auto px-11 text-center mt-10'>
+            <h4 className="text-2xl leading-tight tracking-tight font-bold">เเนะนำตัว</h4> 
+<h5 className="text-2xl leading-tight tracking-tight font-bold">บริษัท เจ ทูลส์ เทคโนโลยี จำกัด</h5>
+    <p className="text-xl leading-tight tracking-tight">เป็นผู้นำเรื่อง การออกแบบงานออโตเมชั่น Automation เครื่องจักรที่ลดเวลาการผลิต และเพิ่มปริมาณงาน และรักษาคุณงานให้ได้ตามมาตราฐาน โดยการใช้เทคโนโลยี ที่เหมาสมและควบคุมได้ง่าย
+    เราเชี่ยวชาญในการผลิตและออกแบบเครื่องจักรอัตโนมัติ เครื่องมือพิเศษในขบวนการผลิตของลูกค้า
+    ด้วยการใช้ ซอพแวร์ชั้นนำที่น่าเชื่อถือ ถูกต้อง มีลิขสิทธิ,ให้บริการผลิตภัณฑ์เครื่องจักรตามความต้องการของลูกค้า,รับผลิตงานตามแบบงานและรับผลิตที่มีความละเอียดสูง</p>          
+          </div>
+          <div className='container mx-auto px-11 text-center mt-10'>
+            <div className="leading-tight tracking-tight">
+<h6 className="text-2xl font-bold ">รายละเอียด</h6>
+<p className="text-xl">DESIGN-MAKING,
+AUTOMATION MACHINE,
+AUTOMATION APPEARANCE,
+CONVEYOR PRODUCT LINE,
+ASSEMBLY AUTO LINE PRODUCTION,
+JIG-FIXTURE FOR INSPECTOIN,
+PICK AND PLACE AUTO MACHANISM,
+PICK AND PLACE AUTO BY ROBOT,
+PCB AUTOMATION LINE,
+AUTOMATION SYSTEM BY ROBOT,
+FABRICATE PART MAKING,
+PRECISION PARTS,
+TOOLS TOOLING,
+ENGINEERING PARTS,
+SPECIAL PARTS,
+EQUIPMENT OTHER,
+TRADING,
+SUPPLY EQUIPMENT AND PART ACCORDING TO CUTOMER NEEDS</p>
+         </div>
+          </div>
+        </section>
+    )
+}
+
+export default About
