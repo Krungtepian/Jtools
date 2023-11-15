938a2e4114b61bfbf1eb7916b5752e3ef5890ad9
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2023-11-15 14:12:46 +0700

    update navbar to be prettier

--- app/(user)/Components/Navigation.tsx
+++ app/(user)/Components/Navigation.tsx
@@ -8,10 +8,10 @@ const Navigation = () => {
     const [navbar, setNavbar] = useState(false);
   return (
       <nav className="w-full bg-white top-0 left-0 right-0 z-10 fixed">
-        <div className="mx-auto md:items-center md:flex lg:px-4 md:px-2 sm:px-2 w-full justify-between lg:text-base ">
-          <div className="flex flex-row items-center justify-between py-3 md:py-5 md:block">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div className="flex  items-center justify-between py-3 md:py-5 md:block">
               <Link href="/" className='max-w-fit'>
-                <h2 className="text-2xl font-semibold w-fit">Jtool logo</h2>
+                <h2 className="text-2xl font-semibold">Jtool logo</h2>
               </Link>
               <div className="md:hidden">
                 <button
@@ -27,23 +27,23 @@ const Navigation = () => {
               </div>
           </div>
           <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
-                <ul className=" md:h-auto items-center flex flex-row w-full justify-end">
-                    <li className=" lg:text-xl md:text-sm sm:text-xs text-black py-1 md:px-5 text-center border-b-2 md:border-b-0  hover:bg-gray-100 hover:text-blue-400 items-center rounded-lg active:scale-105">
+                <ul className="h-screen md:h-auto items-center justify-end md:flex">
+                    <li className=" lg:text-xl md:text-sm sm:text-xs text-black py-1 md:px-5 text-center border-b-2 md:border-b-0  hover:bg-gray-100 hover:text-blue-400 items-center rounded-full active:scale-105">
                       <Link href="/home" onClick={() => setNavbar(!navbar)}>
                         หน้าเเรก
                       </Link>
                     </li>
-                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-lg active:scale-105">
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-full active:scale-105">
                       <Link href="/about-us" onClick={() => setNavbar(!navbar)}>
                         เกี่ยวกับเรา
                       </Link>
                     </li>
-                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-lg active:scale-105">
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400  hover:bg-gray-100 rounded-full active:scale-105">
                       <Link href="/news" onClick={() => setNavbar(!navbar)}>
                         ข่าวสารและกิจกรรม
                       </Link>
                     </li>
-                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100 rounded-lg active:scale-105">
+                    <li className="lg:text-xl md:text-sm sm:text-xs text-black py-1 px-5 text-center  border-b-2 md:border-b-0  hover:text-blue-400 hover:bg-gray-100 rounded-full active:scale-105">
                       <Link href="/service" onClick={() => setNavbar(!navbar)}>
                         สินค้าและบริการ
                       </Link>
