5cc69e9b615a402aae8b097ee50746efa9e23b91
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-11-16 14:20:15 +0700

    Update Aboutus.tsx

--- app/(user)/about-us/Components/Aboutus.tsx
+++ app/(user)/about-us/Components/Aboutus.tsx
@@ -10,27 +10,27 @@ const About = () => {
             <h2 className="text-2xl mt-4">
                 Business detail
                 </h2>
-<strong className="text-xl leading-tight tracking-tight">สโลเเกน</strong>
-<p className="text-xl leading-tight tracking-tight">สร้างสรรค์ผลิตภัณฑ์ที่ดีมีคุณค่าให้ลูกค้าอย่างยั่งยืน</p>
-<strong className="text-xl leading-tight tracking-tight">วิสัยทัศน์</strong>
-<p className="text-xl leading-tight tracking-tight">เพิ่มมูลค่าผลิตภัณฑ์โดยการทำ Automation ให้เป็นที่ยอมรับ ในด้านคุณภาพ บริการ ราคา
-และขยายการขายไปยังกลุ่มลูกค้าใหม่</p>
-<strong className="text-xl leading-tight tracking-tight">พันธกิจ</strong>
-<p className="text-xl leading-tight tracking-tight">นำเทคโนโลยีใหม่ๆมาใช้ตั้งแต่ขบวนการออกแบบ การผลิต และการประกันคุณภาพสินค้าตั้งแต่วัตถุดิบจนเป็นผลิตภัณฑ์</p>
-<p className="text-xl leading-tight tracking-tight">ขยายการไปยังกลุ่มลูกค้าใหม่ๆ อย่างต่อเนื่อง</p>
-<p className="text-xl leading-tight tracking-tight">เสริมสร้างและพัฒนาบุคลากรให้สามารถรับงาน Automation ให้มากที่สุด</p>
-          </div>
-          <div className="container mx-auto px-11 text-center mt-10 text-white">
-<h3 className="text-xl leading-tight tracking-tight font-bold ">หัวข้อสำคัญในการดำเนินงาน</h3>
-<div className="text-xl leading-tight tracking-tight">
-<p><strong>การตลาด</strong>-ขยายฐานการขายอย่างไม่หยุดยั้ง</p>
-<p><strong>คุณภาพ</strong>-สร้างระบบและจัดการคุณภาพของสินค้าผลิต</p>
-<p><strong>เทคโนโลยี</strong>-ใช้เทคโนโลยีที่เหมาะสมและควบคุมได้</p>
-<p><strong>การผลิต</strong>-ยกระดับประสิทธิภาพการผลิต</p>
-<p><strong>ต้นทุน</strong>-ขยายงานการผลิตให้เต็มกำลังความสามารถของคนและเครื่องจักร</p>
-<p><strong>บุคลากร</strong>-สร้างบุคลากรและองค์กรที่แข็งแกร่ง</p>
-<p><strong>อื่นๆ</strong>-ควบคุมความเสี่ยง,รักษา 5ส,รักษา Compliance, Percision</p>               
-</div>
+            <strong className="text-xl leading-tight tracking-tight">สโลเเกน</strong>
+            <p className="text-xl leading-tight tracking-tight">สร้างสรรค์ผลิตภัณฑ์ที่ดีมีคุณค่าให้ลูกค้าอย่างยั่งยืน</p>
+            <strong className="text-xl leading-tight tracking-tight">วิสัยทัศน์</strong>
+            <p className="text-xl leading-tight tracking-tight">เพิ่มมูลค่าผลิตภัณฑ์โดยการทำ Automation ให้เป็นที่ยอมรับ ในด้านคุณภาพ บริการ ราคา
+            และขยายการขายไปยังกลุ่มลูกค้าใหม่</p>
+            <strong className="text-xl leading-tight tracking-tight">พันธกิจ</strong>
+            <p className="text-xl leading-tight tracking-tight">นำเทคโนโลยีใหม่ๆมาใช้ตั้งแต่ขบวนการออกแบบ การผลิต และการประกันคุณภาพสินค้าตั้งแต่วัตถุดิบจนเป็นผลิตภัณฑ์</p>
+            <p className="text-xl leading-tight tracking-tight">ขยายการไปยังกลุ่มลูกค้าใหม่ๆ อย่างต่อเนื่อง</p>
+            <p className="text-xl leading-tight tracking-tight">เสริมสร้างและพัฒนาบุคลากรให้สามารถรับงาน Automation ให้มากที่สุด</p>
+                      </div>
+                      <div className="container mx-auto px-11 text-center mt-10 text-white">
+            <h3 className="text-xl leading-tight tracking-tight font-bold ">หัวข้อสำคัญในการดำเนินงาน</h3>
+            <div className="text-xl leading-tight tracking-tight">
+            <p><strong>การตลาด</strong>-ขยายฐานการขายอย่างไม่หยุดยั้ง</p>
+            <p><strong>คุณภาพ</strong>-สร้างระบบและจัดการคุณภาพของสินค้าผลิต</p>
+            <p><strong>เทคโนโลยี</strong>-ใช้เทคโนโลยีที่เหมาะสมและควบคุมได้</p>
+            <p><strong>การผลิต</strong>-ยกระดับประสิทธิภาพการผลิต</p>
+            <p><strong>ต้นทุน</strong>-ขยายงานการผลิตให้เต็มกำลังความสามารถของคนและเครื่องจักร</p>
+            <p><strong>บุคลากร</strong>-สร้างบุคลากรและองค์กรที่แข็งแกร่ง</p>
+            <p><strong>อื่นๆ</strong>-ควบคุมความเสี่ยง,รักษา 5ส,รักษา Compliance, Percision</p>               
+            </div>
           </div>
           <div className='container mx-auto px-11 text-center mt-10 text-white'>
             <h4 className="text-2xl leading-tight tracking-tight font-bold">เเนะนำตัว</h4> 
