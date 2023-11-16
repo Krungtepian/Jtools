fc32f8418a286f1da6772f5e818aeb6d5c37e2a5
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2023-11-16 14:28:36 +0700

    Config bugs fixed navigation big patch

--- app/(user)/Components/SlideShow.tsx
+++ app/(user)/Components/SlideShow.tsx
@@ -2,16 +2,14 @@
 
 import React from 'react'
 import Carousel from './Carousel'
-
 type Props = {}
 
 export default function SlideShow({}: Props) {
     const images = [
-    "https://cdn.discordapp.com/attachments/1129443288097759322/1155516155872690217/jtool_pic_1.png",
-    "https://cdn.discordapp.com/attachments/1129443288097759322/1155516155872690217/jtool_pic_1.png",
-    "https://cdn.discordapp.com/attachments/1129443288097759322/1155516155872690217/jtool_pic_1.png",
-    "https://cdn.discordapp.com/attachments/1129443288097759322/1155516155872690217/jtool_pic_1.png"
-  ];
+      "@/app/assets/images/jtool_pic_1.png",
+      "@/app/assets/images/IMG_2505.jpg"
+  ]
+
   return (
 <div className="lg:w-3/4 mx-auto my-2"> 
       {/* //carousel-image */}
